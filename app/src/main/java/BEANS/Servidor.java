c27cc876ac85fc8f904594606a62e12c41d7cd44
atack08 <atack08@hotmail.es>
2017-04-23 11:04:37 +0200

    Initial commit

---
+++ app/src/main/java/BEANS/Servidor.java
@@ -0,0 +1,45 @@
+package BEANS;
+
+public class Servidor {
+
+    private String nombreServidor;
+    private String ipServidor;
+    private String paisServidor;
+
+    public Servidor(){}
+
+    public Servidor(String nombreServidor, String ipServidor, String paisServidor) {
+        this.nombreServidor = nombreServidor;
+        this.ipServidor = ipServidor;
+        this.paisServidor = paisServidor;
+    }
+
+    public String getNombreServidor() {
+        return nombreServidor;
+    }
+
+    public void setNombreServidor(String nombreServidor) {
+        this.nombreServidor = nombreServidor;
+    }
+
+    public String getIpServidor() {
+        return ipServidor;
+    }
+
+    public void setIpServidor(String ipServidor) {
+        this.ipServidor = ipServidor;
+    }
+
+    public String getPaisServidor() {
+        return paisServidor;
+    }
+
+    public void setPaisServidor(String paisServidor) {
+        this.paisServidor = paisServidor;
+    }
+
+    @Override
+    public String toString() {
+        return this.nombreServidor + "-" + this.ipServidor;
+    }
+}
---
+++ app/src/main/java/com/example/atack08/groupload_app/ServerSelect.java
@@ -0,0 +1,35 @@
+package com.example.atack08.groupload_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import BEANS.Servidor;
+
+public class ServerSelect extends AppCompatActivity {
+
+    private Spinner comboServidor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_select);
+
+        //RECUPERAMOS EL SPINNER DE SERVIDORES
+        comboServidor = (Spinner)findViewById(R.id.comboServidores);
+
+        //CARGAMOS LOS SERVIDORES EN LA EL COMBO
+        //NOTA - SI HUBIESE MÁS SERVIDORES SE PODRÍA CREAR UN FICHERO XML PARA ALMACENARLOS
+        Servidor s1 = new Servidor("HALL-10000","10.10.120.155","España");
+        Servidor s2 = new Servidor("RedIris","10.10.10.101","España");
+        ArrayList<Servidor> listaS = new ArrayList<>();
+        listaS.add(s1);listaS.add(s2);
+        ArrayAdapter<Servidor> adapter = new ArrayAdapter<>(this,R.layout.layout_spinner_servidor,R.id.descripcionServidor,listaS);
+
+        comboServidor.setAdapter(adapter);
+
+    }
+}
