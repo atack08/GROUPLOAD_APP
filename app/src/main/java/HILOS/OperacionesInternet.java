6c02c2c7582d2d1504d46214d9bfeb6e5929554b
gload2017 <gload2017@gmail.com>
2017-04-28 21:35:18 +0200

    28/04/2017

---
+++ app/src/main/java/HILOS/OperacionesInternet.java
@@ -0,0 +1,18 @@
+package HILOS;
+
+/**
+ * Created by atack08 on 28/4/17.
+ */
+
+public class OperacionesInternet {
+
+    public OperacionesInternet(){
+
+    }
+
+    //MÉTODO PARA COMPROBAR SI UN SERVIDOR ESTÁ ONLINE
+    public void pedirConectividadServidor(){
+
+    }
+
+}
--- app/src/main/java/com/example/atack08/groupload_app/ServerSelect.java
+++ app/src/main/java/com/example/atack08/groupload_app/ServerSelect.java
@@ -1,18 +1,28 @@
 package com.example.atack08.groupload_app;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
 
 import BEANS.Servidor;
 
 public class ServerSelect extends AppCompatActivity {
+
+    //CONSTANTES PARA PERMISOS
+    private final int PERMISO_WRITE_READ_EXTERNAL_STORAGE = 0 ;
 
     private Spinner comboServidor;
 
@@ -33,6 +43,9 @@ public class ServerSelect extends AppCompatActivity {
         ArrayAdapter<Servidor> adapter = new ArrayAdapter<>(this,R.layout.layout_spinner_servidor,R.id.descripcionServidor,listaS);
 
         comboServidor.setAdapter(adapter);
+
+        //COMPROBAMOS PERMISOS
+        comprobarPermisos();
 
     }
 
@@ -63,4 +76,74 @@ public class ServerSelect extends AppCompatActivity {
         dialog.show();
 
     }
+
+    //MÉTODO PARA MOSTRAR INFO
+    public void mostrarPanelPermisos(String msg){
+
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        dialog.setMessage(msg);
+        dialog.setTitle("INFO");
+
+        dialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+        dialog.show();
+
+    }
+
+    //MÉTODO PARA COMPROBAR LOS PERMISOS DEL SISTEMA
+    public void comprobarPermisos(){
+        
+        //COMPROBAMOS PERMISOS
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PERMISO_WRITE_READ_EXTERNAL_STORAGE);
+        }
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISO_WRITE_READ_EXTERNAL_STORAGE);
+        }
+
+
+
+        
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISO_WRITE_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "PERMISO SD CONCEDIDO", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    Toast.makeText(this, "PERMISO SD DENEGADO", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
 }
